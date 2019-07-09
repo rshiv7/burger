0817883d51c66eacec836bbe5d2c8e9ed7a159c8
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-07-08 18:14:46 -0700

    Created Database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Zinger',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('T.F.B',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Umami',true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Filet-o-fish',true);
